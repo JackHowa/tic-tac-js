a125e17fed593710113cb9c820ce822d2796168d
jackhowa <jackhowaa@gmail.com>
2018-03-04 12:10:47 -0600

    Rename constant and add assertions for helper function

--- tic-tac-js.js
+++ tic-tac-js.js
@@ -1,4 +1,4 @@
-const winningSolutions = [
+const WINNING_SOLUTIONS = [
 	// horizontal
 	[0,1,2],
 	[3,4,5],
@@ -12,9 +12,12 @@ const winningSolutions = [
 	[2,4,7]
 ]
 
+// part of the assignment I saw was to figure out how the winner was won 
+function playGame(wholeBoard) {
+	
+}
 
-
-// 
+// simple check for whether array of indices have values in any of the inputted spots 
 function isWinner(inputArray, targetIndex) {
 	let targetValues = inputArray.filter((input, index) => targetIndex.includes(index)); // ["X","X","X"] 
 	return targetValues.every(value => value != ""); // true if there's not empty strings in any of the values
@@ -24,13 +27,13 @@ let targetIndex = [0,1,2];
 
 let inputArray = ["X","X","X","","","","","","","","",""];
 
-console.log(isWinner(inputArray, targetIndex)); // should be true 
+console.assert(isWinner(inputArray, targetIndex)); // should return true with no errors 
 
 let wrongTargetIndex = [0,1,3];
 
 let wrongInputArray = ["X","X","X","","","","","","","","",""];
 
-console.log(isWinner(wrongInputArray, wrongTargetIndex)); // should be false 
+console.assert(isWinner(wrongInputArray, wrongTargetIndex) == false); // should return true with no errors 
 
 
 
