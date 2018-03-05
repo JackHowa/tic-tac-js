e1638da095f286102542c41924d7714f0152b2de
jackhowa <jackhowaa@gmail.com>
2018-03-05 08:00:00 -0600

    Play against a dumb computer

--- index.js
+++ index.js
@@ -10,9 +10,31 @@ function handleClick(e) {
   potentialSlots.forEach(slot => {
     // check if this equal to itself 
     if (slot === this) {
-      slot.innerHTML = "X";
+			slot.innerHTML = "X";
+			slot.classList.add("js-board__cell--filled");
     }
-  });
+	});
+	
+	// could build the computer into this part
+	computerMove();
+}
+
+let openSlots = document.querySelectorAll('.js-board__cell--filled');
+
+function computerMove() {
+	// all the board cells that aren't filled 
+	openSlots = document.querySelectorAll('.js-board__cell:not(.js-board__cell--filled)');
+
+	// subtract one so it's 0 indexed 
+	let potentialIndex = openSlots.length - 1;
+
+	// let randomIndex = Math.floor(Math.random()*10) + 1; 
+
+	// this will always take the last indexed available spot
+	let randomComputerFilledSlot = openSlots.item(potentialIndex);
+	console.log(randomComputerFilledSlot);
+	randomComputerFilledSlot.innerHTML = "O";
+	randomComputerFilledSlot.classList.add("js-board__cell--filled");
 }
 
 const checkSolutionButton = document.querySelector("#js-check-board-button");
@@ -37,9 +59,11 @@ const resetBoardButton = document.querySelector("#js-reset-board-button");
 resetBoardButton.addEventListener('click', handleResetClick);
 
 function handleResetClick(e) {
-	// reset all of the slots
-	potentialSlots.forEach(potentialSlot => {
-		potentialSlot.innerHTML = "_";
+	// only have to reset the filled slots
+	filledSlots = document.querySelectorAll('.js-board__cell--filled');
+	filledSlots.forEach(filledSlot => {
+		filledSlot.innerHTML = "_";
+		filledSlot.classList.remove("js-board__cell--filled");
 	})
 
 	// reset the win message 
