93535765051774da15af6a5dfa6b1048196cc4a9
jackhowa <jackhowaa@gmail.com>
2018-03-06 08:00:00 -0600

    Refactor file structure

---
+++ js/index.js
@@ -0,0 +1,78 @@
+const potentialSlots = document.querySelectorAll('.js-board__cell');
+
+potentialSlots.forEach(potentialSlot => {
+  potentialSlot.addEventListener('click', handleClick);
+});
+
+function handleClick(e) {
+
+	// loop through all of these 
+  potentialSlots.forEach(slot => {
+    // check if this equal to itself 
+    if (slot === this) {
+			slot.innerHTML = "X";
+
+			// could "unfilled" the slots that are open 
+			// findUnfilleds(); 
+			// in this way, the css could keep track of which ones are open without calling each time 
+			slot.classList.add("js-board__cell--filled");
+    }
+	});
+	// before the computer move may want to check for winner
+
+
+	// could build the computer into this part
+	computerMove();
+}
+
+let openSlots = document.querySelectorAll('.js-board__cell--filled');
+
+function computerMove() {
+	// all the board cells that aren't filled 
+	openSlots = document.querySelectorAll('.js-board__cell:not(.js-board__cell--filled)');
+
+	// subtract one so it's 0 indexed 
+	let potentialIndex = openSlots.length - 1;
+
+	// let randomIndex = Math.floor(Math.random()*10) + 1; 
+
+	// this will always take the last indexed available spot
+	let randomComputerFilledSlot = openSlots.item(potentialIndex);
+	console.log(randomComputerFilledSlot);
+	randomComputerFilledSlot.innerHTML = "O";
+	randomComputerFilledSlot.classList.add("js-board__cell--filled");
+}
+
+const checkSolutionButton = document.querySelector("#js-check-board-button");
+checkSolutionButton.addEventListener('click', handleButtonClick);
+
+function handleButtonClick(e) {
+	let wholeBoardValues = [];
+	potentialSlots.forEach(potentialSlot => {
+		wholeBoardValues.push(potentialSlot.innerHTML);
+	})
+	answerWinner(runner(wholeBoardValues));
+}
+
+const checkSolutionOutput = document.querySelector("#js-how-win");
+
+function answerWinner(answer) {
+	checkSolutionOutput.innerHTML = answer;
+}
+
+// reset 
+const resetBoardButton = document.querySelector("#js-reset-board-button");
+resetBoardButton.addEventListener('click', handleResetClick);
+
+function handleResetClick(e) {
+	// only have to reset the filled slots
+	filledSlots = document.querySelectorAll('.js-board__cell--filled');
+	filledSlots.forEach(filledSlot => {
+		filledSlot.innerHTML = "_";
+		filledSlot.classList.remove("js-board__cell--filled");
+	})
+
+	// reset the win message 
+	checkSolutionOutput.innerHTML = "";
+}
+
---
+++ js/tic-tac-js.js
@@ -0,0 +1,93 @@
+const WINNING_SOLUTIONS = [
+	// horizontal
+	[0,1,2],
+	[3,4,5],
+	[6,7,9],
+	// vertical
+	[0,3,6],
+	[1,4,7],
+	[2,5,9],
+	// diagonal
+	[0,4,9],
+	[2,4,7]
+]
+
+function determineHowWon(indexWinner) {
+	switch(indexWinner) {
+		case 0:
+		case 1:
+		case 2:
+			return "Won via horizontal";
+			break;
+		case 3:
+		case 4:
+		case 5:
+			return "Won via vertical";
+			break;
+		case 6:
+		case 7:
+			return "Won via diagonal";
+			break;
+		default:
+			return "No winner or unrecognized solution";
+	}
+}
+
+// part of the assignment I saw was to figure out how the winner was won 
+function playGame(wholeBoard) {
+	let indexWinner;
+	for (let i = 0; i < WINNING_SOLUTIONS.length; i++) {
+		if (isWinner(wholeBoard, WINNING_SOLUTIONS[i]) == true) {
+			indexWinner = i;
+
+			// crude for now
+			// refactor: could use a while loop
+			break;
+		}
+	}
+	return indexWinner;
+}
+
+// simple check for whether array of indices have values in any of the inputted spots 
+function isWinner(inputArray, targetIndex) {
+	let targetValues = inputArray.filter((input, index) => targetIndex.includes(index)); // ["X","X","X"] 
+
+	// currently take in everything except for default front-end value of _
+	// will have to figure out who won not just that there's a winner 
+	// may need to make this isWinner function more of a decider per person 
+	// like each person's move will have an is winner attached to it
+	// will have to pass in the target character into the parameter 
+	// for instance, could be default x 
+	// because currently it 
+	return targetValues.every(value => value != "" && value != "_"); 
+};
+
+function runner(wholeBoard) {
+	let winningIndex = playGame(wholeBoard);
+	return determineHowWon(winningIndex);
+}
+
+let targetIndex = [0,1,2];
+
+// make sure there's only nine spots in the input array
+// will find an undefined index based on indices of solutions otherwise 
+let inputArray = ["X","X","X","","","","","",""];
+
+console.assert(isWinner(inputArray, targetIndex)); // should return true with no errors 
+
+let wrongTargetIndex = [0,1,3];
+
+console.assert(isWinner(inputArray, wrongTargetIndex) == false); // should return true with no errors 
+
+console.assert(playGame(inputArray) === 0); // finds at index 0 the solution based on previous variables
+
+// let's see if it can find a latter solution 
+let lateWinnerInputArray = ["","","","","","","X","X","X"];
+console.assert(playGame(lateWinnerInputArray) === 2); // this would be the last horizontal index 
+
+let winningIndex = 2;
+console.assert(determineHowWon(winningIndex) === "Won via horizontal");
+
+// [2,4,7]
+let diagonalWinnerInputArray = ["","","X","","X","","","X",""];
+console.assert(runner(diagonalWinnerInputArray) === "Won via diagonal");
